fab43623ba59c5a95482b74352056e9251207f76
luke <lukeistein@gmail.com>
2022-05-26 13:39:32 -0700

    removed unneccessary try catch

--- main.cpp
+++ main.cpp
@@ -12,31 +12,27 @@ using namespace std;
 int main(int, char** argv) {
     cout << argv[0] << " build " << __DATE__ << " " << __TIME__ << endl;
     inode_state state;
-    try {
-        for (;;) {
-            try {
-                // read a line, break at EOF
-                cout << state.get_prompt();
-                string line;
-                getline(cin, line);
-                if (cin.eof()) {
-                    cout << endl;
-                    break;
-                }
-                // split the line into words and lookup the function
-                vector<string> words = split(line, " \t");
-                if (words.at(0) == "#")
-                    continue;
-                cmd_fn fn = find_cmd_fn(words.at(0));
-                fn(state, words);
-            } catch (file_error& error) {
-                cerr << error.what() << endl;
-            } catch (command_error& error) {
-                cerr << error.what() << endl;
+    for (;;) {
+        try {
+            // read a line, break at EOF
+            cout << state.get_prompt();
+            string line;
+            getline(cin, line);
+            if (cin.eof()) {
+                cout << endl;
+                break;
             }
+            // split the line into words and lookup the function
+            vector<string> words = split(line, " \t");
+            if (words.at(0) == "#")
+                continue;
+            cmd_fn fn = find_cmd_fn(words.at(0));
+            fn(state, words);
+        } catch (file_error& error) {
+            cerr << error.what() << endl;
+        } catch (command_error& error) {
+            cerr << error.what() << endl;
         }
-    } catch (shell_exit&) {
     }
-
     return EXIT_SUCCESS;
 }
